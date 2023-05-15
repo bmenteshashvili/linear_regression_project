b82fb8c9678ab7401f64cb7f452f335e53377411
beso <bmenteshashvili@gmail.com>
2023-05-15 14:55:21 +0400

    add main file

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import seaborn as sbn
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show()
